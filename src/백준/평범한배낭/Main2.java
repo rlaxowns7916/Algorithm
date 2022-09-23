6b7fd23375b80e32bce4721d2895d9118d1f35f3
ktj1997 <rlaxowns7916@gmail.com>
2022-09-23 23:24:46 +0900

    평범한 배낭

--- src/백준/평범한배낭/Main2.java
+++ src/백준/평범한배낭/Main2.java
@@ -3,66 +3,51 @@ package 백준.평범한배낭;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main2 {
-    static class Item implements Comparable<Item> {
-        int value;
-        int weight;
-
-        public Item(int value,int weight){
-            this.value = value;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Item o) {
-            return this.weight - o.weight;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
-        /**
-         * N: 물품의 수
-         * K: 버틸 수 있는 무게
-         *
-         * W: 물건의 무게
-         * V: 물건의 가치
-         */
-        int N = 0, K = 0, W = 0, V = 0;
-
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
 
-        Item[] items = new Item[N];
-        for (int i = 0; i < N; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][k + 1];
+        ArrayList<Item> items = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
-            W = Integer.parseInt(st.nextToken());
-            V = Integer.parseInt(st.nextToken());
-
-            items[i] = new Item(V,W);
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            Item item = new Item(weight, value);
+            items.add(item);
         }
-        Arrays.sort(items);
-        System.out.println(solve(N, K, items));
-    }
 
-    public static int solve(int N, int K, Item[] items) {
-        int[][] dp = new int[N + 1][K + 1];
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
-                if (items[i - 1].weight <= j)
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i - 1][j - items[i-1].weight] + items[i-1].value);
-                else
-                    dp[i][j] = dp[i-1][j];
+        for (int i = 1; i <= n; i++) {
+            Item target = items.get(i - 1);
+            int weight = target.weight;
+            int value = target.value;
+            for (int j = 1; j <= k; j++) {
+                if (weight <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j - weight] + value, dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return dp[N][K];
+        System.out.println(dp[n][k]);
+    }
+}
+
+class Item {
+    int weight;
+    int value;
+
+    public Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
     }
 }
