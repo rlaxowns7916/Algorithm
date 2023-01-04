80e6fa5fb2f87f173e10e0de42eea7a570c35b75
ktj1997 <rlaxowns7916@gmail.com>
2023-01-04 18:23:51 +0900

    0104

---
+++ src/programmers/SQL/카테고리별도서판매량집계하기/import.sql
@@ -0,0 +1,15 @@
+SELECT
+    b.CATEGORY as CATEGORY,
+    SUM(SALES) as TOTAL_SALES
+FROM
+    BOOK b
+        JOIN
+    BOOK_SALES bs
+    ON
+            b.BOOK_ID = bs.BOOK_ID
+WHERE
+        DATE_FORMAT(bs.SALES_DATE,"%Y-%m") ="2022-01"
+GROUP BY
+    b.CATEGORY
+ORDER BY
+    b.CATEGORY
