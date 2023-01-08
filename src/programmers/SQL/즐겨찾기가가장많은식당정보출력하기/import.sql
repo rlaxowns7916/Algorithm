308808c8bc7f54d269d468ebba4ef9209b1dd25d
ktj1997 <rlaxowns7916@gmail.com>
2023-01-08 18:17:20 +0900

    0108

--- src/programmers/SQL/즐겨찾기가가장많은식당정보출력하기/import.sql
+++ src/programmers/SQL/즐겨찾기가가장많은식당정보출력하기/import.sql
@@ -1,5 +1,4 @@
 -- 코드를 입력하세요
-
 SELECT
     ori.FOOD_TYPE,
     ori.REST_ID,
@@ -10,15 +9,15 @@ FROM
         JOIN
     (
         SELECT
-            iri.FOOD_TYPE as FOOD_TYPE,
-            MAX(iri.FAVORITES) as FAVORITES
+            REST_ID,
+            FOOD_TYPE,
+            RANK() OVER(PARTITION BY iri.FOOD_TYPE ORDER BY iri.FAVORITES DESC) as RANKING
         FROM
             REST_INFO iri
-        GROUP BY
-            iri.FOOD_TYPE
-    )t
+    ) t
     ON
-                ori.FOOD_TYPE = t.FOOD_TYPE
-            AND ori.FAVORITES = t.FAVORITES
+            ori.REST_ID = t.REST_ID
+WHERE
+        RANKING = 1
 ORDER BY
     FOOD_TYPE DESC
