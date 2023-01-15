925f22fd859fdda415948eb5db1439ef60759613
ktj1997 <rlaxowns7916@gmail.com>
2023-01-15 12:56:50 +0900

    0115

--- src/programmers/SQL/자동차대여기록별금액구하기/import.sql
+++ src/programmers/SQL/자동차대여기록별금액구하기/import.sql
@@ -1,38 +1,11 @@
--- 코드를 입력하세요
 SELECT
-    t.HISTORY_ID,
-    (
-        CASE
-            WHEN t.RENTAL_RANGE < 7 THEN t.DAILY_FEE * t.RENTAL_RANGE
-            ELSE
-                (
-                        t.RENTAL_RANGE * t.DAILY_FEE *
-                        (
-                            SELECT
-                                    1 - (it.DISCOUNT_RATE * 0.01)
-                            FROM
-                                (
-                                    SELECT
-                                        LEFT(DISCOUNT_RATE,LENGTH(DISCOUNT_RATE-1)) as DISCOUNT_RATE
-                                    FROM
-                                        CAR_RENTAL_COMPANY_DISCOUNT_PLAN cdp
-                                    WHERE
-                                            cdp.CAR_TYPE = t.CAR_TYPE
-                                      AND
-                                            cdp.DURATION_TYPE =
-                                            (
-                                                CASE
-                                                    WHEN t.RENTAL_RANGE < 30 THEN '7일 이상'
-                                                    WHEN t.RENTAL_RANGE < 90 THEN '30일 이상'
-                                                    ELSE '90일 이상'
-                                                    END
-                                                )
-                                ) as it
-                        )
-                    )
-            END
-        ) as FEE
-
+    HISTORY_ID,
+    ROUND((
+              CASE
+                  WHEN RENTAL_RANGE < 7 THEN 1
+                  ELSE 1 - (DISCOUNT_RATE *0.01)
+                  END
+              ) * RENTAL_RANGE * DAILY_FEE) as FEE
 FROM
     (
         SELECT
@@ -49,4 +22,15 @@ FROM
         WHERE
                 c.CAR_TYPE = '트럭'
     ) t
+        LEFT JOIN
+    CAR_RENTAL_COMPANY_DISCOUNT_PLAN cdp
+    ON
+                cdp.CAR_TYPE = t.CAR_TYPE
+            AND (
+                    CASE
+                        WHEN t.RENTAL_RANGE < 30 THEN '7일 이상'
+                        WHEN t.RENTAL_RANGE < 90 THEN '30일 이상'
+                        ELSE '90일 이상'
+                        END
+                    ) = cdp.DURATION_TYPE
 ORDER BY FEE DESC, HISTORY_ID DESC
