33a4d9fccb3eec607e720a3176e2477651b1d670
ktj1997 <rlaxowns7916@gmail.com>
2024-09-18 11:12:55 +0900

    0918

---
+++ src/파이썬/파이썬300/P51-60.py
@@ -0,0 +1,37 @@
+# 51
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+
+# 52
+movie_rank.append("배트맨")
+
+# 53
+movie_rank.insert(1, "슈퍼맨")
+
+# 54
+del movie_rank[3]
+
+# 55
+del movie_rank[2]
+del movie_rank[2]
+
+# 56
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+
+print(lang1 + lang2)
+
+# 57
+nums = [1, 2, 3, 4, 5, 6, 7]
+print(max(nums), min(nums))
+
+# 58
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
+
+# 59
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+print(len(cook))
+
+# 60
+nums = [1, 2, 3, 4, 5]
+print(sum(nums)/len(nums))
